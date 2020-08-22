01bb6b1462f7f34306f9ec49656f9e42e9efbac9
Rohit <rohitsrao@gmail.com>
2020-08-22 18:12:24 +0200

    Added installation command for tldr

--- program_install_script.sh
+++ program_install_script.sh
@@ -12,7 +12,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 #Installing  applications
-sudo apt install -yy build-essential curl vim git timeshift vlc tracker htop net-tools texlive-latex-extra texmaker gnome-tweaks owncloud-client locate nodejs npm youtube-dl
+sudo apt install -yy build-essential curl vim git timeshift vlc tracker htop net-tools texlive-latex-extra texmaker gnome-tweaks owncloud-client locate nodejs npm youtube-dl tldr
 
 sudo snap mp3gain
 
