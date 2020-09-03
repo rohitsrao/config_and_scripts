eb8b9a46819863fa6c8196e42a246800ba7a932e
Rohit <rohitsrao@gmail.com>
2020-09-03 06:58:41 +0200

    copies vimrc from ./vim to home

--- program_install_script.sh
+++ program_install_script.sh
@@ -42,6 +42,9 @@ sudo mkdir /home/$USER/.vim/autoload
 sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+#Copy vimrc
+cp ./vim/vimrc ~/.vimrc
+
 #Installing brave browser
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
