2a1aca4f906d4878791ff29fbb0f85cff9bcf612
Rohit <rohitsrao@gmail.com>
2021-05-30 08:55:23 +0200

    Renamed vim_update.sh to copy_to_config.sh

---
+++ copy_to_config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#This script is to copy files from the ~/.vim/ director and the ~/.vimrc
+#into the config_and_scripts folder and then push to github
+
+#Copy vimrc
+sudo cp ~/.vimrc ./vim/vimrc
+echo ".vimrc successfully copied to ./vim/vimrc"
+
+#Copy UltiSnips directory
+sudo cp -a UltiSnips/ ~/Documents/config_and_scripts/vim/UltiSnips/
+echo "UltiSnips Directory copied"
