0a66919fc7577e0ae592d3a383ea0ea9e237cef1
Rohit <rohitsrao@gmail.com>
2021-05-30 09:08:22 +0200

    Improved verbose text output and added git status call

--- copy_to_config.sh
+++ copy_to_config.sh
@@ -9,4 +9,10 @@ echo ".vimrc successfully copied to ./vim/vimrc"
 
 #Copy UltiSnips directory
 sudo cp -a ~/.vim/UltiSnips/ ~/Documents/config_and_scripts/vim/
-echo "UltiSnips Directory copied"
+echo "UltiSnips Directory copied successfully"
+
+#Execute git status command
+echo ""
+echo "Running git status"
+git status
+
